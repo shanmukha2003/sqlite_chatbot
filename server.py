c3a24006e0bbba5d00b3840fcbd319904bcc4212
shanmukha2003 <penukondashanmukha@karunya.edu.in>
2025-02-01 17:31:48 +0530

    Fixed start command for Railway

--- server.py
+++ server.py
@@ -95,4 +95,4 @@ def get_db_structure():
 
 if __name__ == "__main__":
     print(f"🚀 Server running on http://127.0.0.1:5000/")
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
